abe0ac8a6b2c5e581e94752458d3d4a4902dd72a
Soldier : PePe <ahmed.m.alnajjar1@gmail.com>
2020-05-03 21:01:49 +0300

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @brief		API bootstrap
+ * @author		<a href='https://www.invisioncommunity.com'>Invision Power Services, Inc.</a>
+ * @copyright	(c) Invision Power Services, Inc.
+ * @license		https://www.invisioncommunity.com/legal/standards/
+ * @package		Invision Community
+ * @since		18 Feb 2013
+ */
+
+require_once '../init.php';
+\IPS\Dispatcher\Api::i()->run();
